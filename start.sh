bc3a0ba7e32c1e42546d3e06ba547cda83387bab
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-01-24 12:54:05 -0800

    slick start script

--- start.sh
+++ start.sh
@@ -1,5 +1,18 @@
+#!/bin/bash
+trap 'killall' INT
+
+# https://unix.stackexchange.com/questions/55558/how-can-i-kill-and-wait-for-background-processes-to-finish-in-a-shell-script-whe
+
+killall() {
+    trap '' INT TERM # ignore INT and TERM while shutting down
+    echo "shutting down..."
+    kill -TERM 0
+    wait
+    echo "done"
+}
+
 (cd backend/cmd/roxie/  && go run main.go) &
 (cd backend/cmd/site/   && go run main.go) &
 (cd backend/cmd/hats/   && go run main.go) &
-wait
-echo
+
+cat
--- stop.sh
+++
@@ -1,1 +0,0 @@
-for pid in $(ps aux | grep "go-build.*main" | grep -v "grep" | awk '{print $2}'); do kill $pid; done
