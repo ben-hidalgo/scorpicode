f1daf22cb1029f02dacf662dc8bf11bc670a2795
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-02-06 13:10:15 -0800

    added redigo

--- backend/go.mod
+++ backend/go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/golang/protobuf v1.3.2
+	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
 	github.com/pkg/errors v0.9.0 // indirect
 	github.com/rs/xid v1.2.1
---
+++ backend/internal/hats/repo/redis/redis.go
@@ -0,0 +1,48 @@
+package redis
+
+import (
+	"backend/internal/hats/repo"
+	"context"
+
+	_ "github.com/gomodule/redigo/redis"
+	"github.com/sirupsen/logrus"
+)
+
+//Repo implements repo.HatRepo
+type Repo struct{}
+
+// enforces the interface is implemented
+var _ repo.HatRepo = (*Repo)(nil)
+
+// NewRepo returns a pointer to a new instance of Repo
+func NewRepo() *Repo {
+	return &Repo{}
+}
+
+//FindAll queries all records
+func (r *Repo) FindAll(limit repo.Limit, offset repo.Offset) (hats []*repo.HatMod, err error) {
+	return hats, nil
+}
+
+//Save performs an upsert
+func (r *Repo) Save(hm *repo.HatMod) error {
+	return nil
+}
+
+//BeginTx implements HatRepo.BeginTxn()
+func (r *Repo) BeginTxn(ctx context.Context) error {
+	logrus.Debug("redis.BeginTxn()")
+	return nil
+}
+
+//Rollback implements HatRepo.Rollback()
+func (r *Repo) Rollback() error {
+	logrus.Debug("redis.Rollback()")
+	return nil
+}
+
+//Commit implements HatRepo.Commit()
+func (r *Repo) Commit() error {
+	logrus.Debug("redis.Commit()")
+	return nil
+}
