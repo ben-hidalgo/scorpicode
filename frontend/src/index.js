311e5fd102283f0bb327f9c011693e35a382aaa2
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-02-27 15:13:24 -0800

    upgraded mobx libs

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,6 @@ import ReactDOM from "react-dom";
 import promiseFinally from "promise.prototype.finally";
 import React from "react";
 import { HashRouter } from "react-router-dom";
-import { useStrict } from "mobx";
 import { Provider } from "mobx-react";
 
 import App from "./App";
@@ -26,7 +25,6 @@ window._____APP_STATE_____ = stores;
 
 promiseFinally.shim();
 
-useStrict(true);
 
 ReactDOM.render(
   <Provider {...stores}>
