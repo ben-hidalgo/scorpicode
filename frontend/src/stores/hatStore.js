a81f66a0c8560727e856d3ea0c6a2d9364b87144
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-03-06 13:27:20 -0800

    make hat saves to list

--- frontend/src/stores/hatStore.js
+++ frontend/src/stores/hatStore.js
@@ -11,6 +11,7 @@ class HatStore {
     })
   } // constructor
 
+  // returns all hats
   listHats = () => {
     this.isLoading = true
     agent.Hats.listHats()
@@ -21,8 +22,11 @@ class HatStore {
     this.isLoading = false  
   } // listHats
 
+  // saves a new hat
   makeHat = (color, size, units, style) => {
     
+    this.isLoading = false
+
     let inches = 0
 
     switch(units) {
@@ -36,7 +40,19 @@ class HatStore {
         inches = size
     }
     agent.Hats.makeHat(inches, color, style)
-  } // listHats
+    .then(({ hat }) => {
+      
+      var tempHats = []
+      // add to the front of the list
+      tempHats.push(hat)
+      
+      // for some reason added an element doesn't trigger a render
+      this.hats.forEach(h => tempHats.push(h))
+      this.hats = tempHats
+    })
+    .finally(() => { this.isLoading = false })
+
+  } // makeHat
 
 } // HatStore
 
