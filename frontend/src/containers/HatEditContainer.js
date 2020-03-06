3151176243c7dd0fe670e64b88bb6d9cace9d8cf
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-03-05 16:17:39 -0800

    factored hat color list into function

--- frontend/src/containers/HatEditContainer.js
+++ frontend/src/containers/HatEditContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { extendObservable, action } from 'mobx'
+import { extendObservable } from 'mobx'
 import './HatEditContainer.css'
 import { observer }  from 'mobx-react'
 
@@ -15,41 +15,50 @@ class HatEditContainer extends Component {
 
   render() {
 
-    const colors = [
-      {value: 'RED', text: 'Red'},
-      {value: 'BLUE', text: 'Blue'},
-      {value: 'GREEN', text: 'Green'},
-      {value: 'YELLOW', text: 'Yellow'},
-      {value: 'PURPLE', text: 'Purple'},
-      {value: 'BLACK', text: 'Black'},
-      {value: 'GREY', text: 'Grey'},
-      {value: 'ORANGE', text: 'Orange'},
-    ]
-    
     return (
       <div className="HatEditContainer">
-        <label>
-          <select onChange={this.handleColorChange}>
-            {
-              colors.map(color => {
-                return (
-                  <option key={color.value} value={color.value} >{color.text}</option>
-                )
-              })              
-            }
-          </select>
-          Color
-        </label>
-        
+        <span>_color_ {this.color}</span>
+        <br/>
+        <HatColors hec={this}/>
+        <br/>
         <button onClick={() => {console.log('save')}} type="button">Save</button>
         <button onClick={() => {console.log('cancel')}} type="button">Cancel</button>
       </div>
     )
   }
 
-  handleColorChange = (changeEvent) => {
-    this.color = changeEvent.target.value
-  }
-
 }
+
+function HatColors(props) {
+
+  return (
+      <label>
+        <select onChange={(ce) => {props.hec.color = ce.target.value}}>
+          {
+            props.colors.map(color => {
+              return (
+                <option key={color.value} value={color.value} >{color.text}</option>
+              )
+            })              
+          }
+        </select>
+        Color
+      </label>
+  )
+}
+HatColors.defaultProps = {
+  colors: [
+    {value: 'RED', text: 'Red'},
+    {value: 'BLUE', text: 'Blue'},
+    {value: 'GREEN', text: 'Green'},
+    {value: 'YELLOW', text: 'Yellow'},
+    {value: 'PURPLE', text: 'Purple'},
+    {value: 'BLACK', text: 'Black'},
+    {value: 'GREY', text: 'Grey'},
+    {value: 'ORANGE', text: 'Orange'},
+  ],
+
+};
+
+
 export default observer(HatEditContainer)
