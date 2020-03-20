4025a977323ad3564251792d4de8df5f3b9b57dc
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-03-20 14:44:52 -0700

    simplified website

--- website/src/components/footer.js
+++ website/src/components/footer.js
@@ -1,66 +1,20 @@
 import React from 'react'
-import { FaTwitter, FaGithub, FaMedium } from 'react-icons/fa'
-import { StaticQuery, graphql } from 'gatsby'
+import { FaTwitter, FaInstagram } from 'react-icons/fa'
 import './style.scss'
-import Emoji from './emoji'
 
 const Footer = () => (
-  <StaticQuery
-    query={graphql`
-      query SocialQuery {
-        site {
-          siteMetadata {
-            gatsby
-            bulma
-            twitter
-            medium
-            github
-          }
-        }
-      }
-    `}
-    render={data => (
-      <footer className='footer center has-background-light'>
-        <div className='content has-text-centered'>
-          <p className='is-size-4'>
-            This website was handcrafted with plenty cups of{' '}
-            <Emoji emoji='☕' />
-          </p>
-          <p className='is-size-4'>
-            By Aman Mittal (@amanhimself) using{' '}
-            <a href={data.site.siteMetadata.gatsby}>Gatsby</a> +{' '}
-            <a href={data.site.siteMetadata.bulma}>Bulma</a>
-          </p>
-          <article className='media center'>
-            <span className='icon'>
-              <a href={data.site.siteMetadata.twitter}>
-                <FaTwitter size='fa-2x' color='blue' />
-              </a>
-            </span>
-            &nbsp;
-            <span className='icon'>
-              <a href={data.site.siteMetadata.github}>
-                <FaGithub size='fa-2x' color='black' />
-              </a>
-            </span>
-            &nbsp;
-            <span className='icon'>
-              <a href={data.site.siteMetadata.medium}>
-                <FaMedium size='fa-2x' color='green' />
-              </a>
-            </span>
-            &nbsp;
-          </article>
-          &nbsp;
-          <p className='is-size-5'>
-            You can also back or support this project for me to keep it updated
-            by{' '}
-            <a href='https://www.paypal.me/amanhimself/2'>Buying Me a Coffee</a>
-          </p>
-        </div>
-      </footer>
-    )}
-  />
+    <footer className="footer columns is-vcentered is-centered">
+        <span>
+            &copy; Scorpicode 2020
+        </span>
+        <span>&nbsp;&nbsp;&nbsp;</span>
+        <a href="https://www.instagram.com/scorpicode" >
+            <FaInstagram className="no-underline" size="29px" />
+        </a>
+        <a href="https://twitter.com/BenHidalgo8" >
+            <FaTwitter size="29px" />
+        </a>
+    </footer>
 )
 
 export default Footer
