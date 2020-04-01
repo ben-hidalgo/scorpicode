9f53a8148cc39570d130c9f02a77bc7667f89cd3
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-04-01 12:18:26 -0700

    added copyright to footer

--- frontend/src/globals/Footer.js
+++ frontend/src/globals/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { observer }  from 'mobx-react'
-import { FaTwitter, FaInstagram } from 'react-icons/fa'
+import { FaTwitter, FaInstagram, FaCopyright } from 'react-icons/fa'
 
 const Footer = () => {
 
@@ -8,7 +8,7 @@ const Footer = () => {
     <div>
       <div className="content has-text-centered">
         <p>
-          <strong>Scorpicode</strong> by <a href="https://github.com/ben-hidalgo">Ben Hidalgo</a>
+          Scorpicode <FaCopyright /> by <a href="https://github.com/ben-hidalgo">Ben Hidalgo</a>
           <a href="https://www.instagram.com/scorpicode" >
             <FaInstagram size="29px" />
           </a>
