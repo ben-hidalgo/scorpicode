b271521bc0a82beaea62437990f23403bf7922d1
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-05-07 16:57:48 -0700

    working txn create hats from listener

--- backend/internal/hats/hatrabbit/hat_rabbit.go
+++ backend/internal/hats/hatrabbit/hat_rabbit.go
@@ -5,6 +5,7 @@ import (
 	"backend/internal/hats/orderdao"
 	"backend/pkg/rabbit"
 	"context"
+	"encoding/json"
 
 	"github.com/sirupsen/logrus"
 	"github.com/socifi/jazz"
@@ -56,6 +57,39 @@ func ProcessHatsOrderCreated(ctx context.Context, msg []byte) error {
 	logrus.Infof("hatrabbit.ProcessHatsOrderCreated() hd=%#v", hd)
 	logrus.Infof("hatrabbit.ProcessHatsOrderCreated() od=%#v", od)
 	logrus.Infof("hatrabbit.ProcessHatsOrderCreated() rmq=%#v", rmq)
+
+	var order orderdao.Order
+	err := json.Unmarshal(msg, &order)
+	if err != nil {
+		return err
+	}
+	logrus.Infof("hatrabbit.ProcessHatsOrderCreated() order=%#v", order)
+
+	// this is the transaction func
+	tf := func() error {
+
+		// save a hat per quantity
+		for i := int32(0); i < order.Quantity; i++ {
+			hat := &hatdao.Hat{
+				Color: order.Color,
+				Style: order.Style,
+				Size:  order.Size,
+			}
+			err := hd.Create(ctx, hat)
+			if err != nil {
+				return err
+			}
+
+			// TODO: send
+			// rmq.SendJSON()
+		}
+		return nil
+	}
+
+	err = hd.VisitTxn(ctx, tf)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
