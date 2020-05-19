c532598480b2c3fa50cc6d0d24b46d67e8faef08
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-05-19 12:53:51 -0700

    handling queue logic in socket publish

--- frontend/src/globals/Socket.js
+++ frontend/src/globals/Socket.js
@@ -3,18 +3,27 @@ import Websocket from 'react-websocket';
 
 import StoreContext from '../storeContext'
 
+const handleData = (data, hatStore) => {
+
+  let msg = JSON.parse(data);
+  
+  if (msg.queue === 'soxie_hat_created') {
+    hatStore.appendHat(msg.data)
+  }
+}
+
 const Socket = () => {
 
   let {
-    hatStore,
     configStore,
+    hatStore,
   } = React.useContext(StoreContext)
 
   return (
     <div>
       <Websocket 
         url={configStore.getSocketUrl()}
-        onMessage={hatStore.appendHat}
+        onMessage={(data) => {handleData(data, hatStore)}}
         debug={configStore.getSocketDebugBool()}
       />
     </div>
