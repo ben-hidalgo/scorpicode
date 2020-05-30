9854066a8e9564d3db7b10298f31d4318da0c5c3
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-05-29 19:01:05 -0700

    added docker build to actions

---
+++ devops/scripts/docker-build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -exuo pipefail
+
+docker build . -f devops/dockerfiles/hats.Dockerfile     -t hats:`./devops/scripts/go-checksum.sh hats`
+docker build . -f devops/dockerfiles/website.Dockerfile  -t website:`./devops/scripts/js-checksum.sh website`
+docker build . -f devops/dockerfiles/frontend.Dockerfile -t frontend:`./devops/scripts/js-checksum.sh frontend`
+docker build . -f devops/dockerfiles/roxie.Dockerfile    -t roxie:`./devops/scripts/go-checksum.sh roxie`
+docker build . -f devops/dockerfiles/soxie.Dockerfile    -t soxie:`./devops/scripts/go-checksum.sh soxie`
+docker build . -f devops/dockerfiles/debugger.Dockerfile -t debugger:latest
