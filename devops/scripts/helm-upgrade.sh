b52f3d64cc1930eedc0e0cad259567dab7a936f5
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-06-02 14:55:29 -0700

    added helm upgrade to actions

--- devops/scripts/helm-upgrade.sh
+++ devops/scripts/helm-upgrade.sh
@@ -4,9 +4,7 @@ set -exuo pipefail
 #sops -d ./devops/helmchart/local.sops.yaml > ./devops/helmchart/local.plain.yaml
 
 helm upgrade --install scorpicode ./devops/helmchart \
--f devops/helmchart/local.yaml \
--f devops/helmchart/local.plain.yaml \
--f devops/helmchart/tags.yaml \
+-f devops/helmchart/dev.yaml \
 --set common.auth0RedirectUri=http://`minikube ip`:30080/callback \
 --set roxie.loginSuccessTarget=http://`minikube ip`:30080/sc \
 --set frontend.socketHost=`minikube ip`:30081
