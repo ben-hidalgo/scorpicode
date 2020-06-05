4f5f464de4bef8291613e2e83204fb7369ab435d
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-06-05 14:23:31 -0700

    tweaked write tags to output yaml

--- devops/scripts/write-tags.sh
+++ devops/scripts/write-tags.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -exuo pipefail
 
-echo hats.tag=`./devops/scripts/go-checksum.sh hats` >> ./devops/helmchart/tags.yaml
-echo roxie.tag=`./devops/scripts/go-checksum.sh roxie` >> ./devops/helmchart/tags.yaml
-echo soxie.tag=`./devops/scripts/go-checksum.sh soxie` >> ./devops/helmchart/tags.yaml
-echo website.tag=`./devops/scripts/js-checksum.sh website` >> ./devops/helmchart/tags.yaml
-echo frontend.tag=`./devops/scripts/js-checksum.sh frontend` >> ./devops/helmchart/tags.yaml
+echo hats.tag: `./devops/scripts/go-checksum.sh hats` >> ./devops/helmchart/tags.yaml
+echo roxie.tag: `./devops/scripts/go-checksum.sh roxie` >> ./devops/helmchart/tags.yaml
+echo soxie.tag: `./devops/scripts/go-checksum.sh soxie` >> ./devops/helmchart/tags.yaml
+echo website.tag: `./devops/scripts/js-checksum.sh website` >> ./devops/helmchart/tags.yaml
+echo frontend.tag: `./devops/scripts/js-checksum.sh frontend` >> ./devops/helmchart/tags.yaml
