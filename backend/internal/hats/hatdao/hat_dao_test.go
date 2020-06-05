4da6cec075656a8fb69e862fb34c27faff44de72
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-06-05 13:33:26 -0700

    mongo skeleton integration test with sleep before wait

---
+++ backend/internal/hats/hatdao/hat_dao_test.go
@@ -0,0 +1,72 @@
+package hatdao_test
+
+import (
+	"backend/internal/hats/hatdao"
+	"backend/pkg/mongodb"
+	"context"
+	"testing"
+	"time"
+)
+
+const (
+	DefaultColor    = "RED"
+	DefaultStyle    = "FEDORA"
+	DefaultSize     = "06000"
+	DefaultQuantity = 10
+	DefaultNotes    = "Lorem ipsum"
+	DefaultHexID    = "5e8e20bbe6b38b8cb0870808"
+	DefaultSubject  = "google-oauth2|204673116516641832842"
+)
+
+var DefaultCreatedAt = time.Date(2020, time.January, 0, 0, 0, 0, 0, time.UTC)
+var DefaultUpdatedAt = time.Date(2020, time.January, 1, 1, 1, 1, 1, time.UTC)
+
+/*
+func startHat(mock *orderdaomock.Mock, rm *rabbitmock.Mock) (context.Context, *hatserver.Server, *hatspb.MakeHatsRequest) {
+
+	ctx := context.Background()
+
+	ctx = context.WithValue(ctx, orderdao.Key, mock)
+
+	ctx = context.WithValue(ctx, rabbit.Key, rm)
+
+	ctx = context.WithValue(ctx, authnz.Key, &authnz.BearerToken{
+		CC: &authnz.CustomClaims{
+			Roles: []authnz.Role{
+				authnz.HABERDASHER,
+			},
+			Claims: jwt.Claims{
+				Subject: DefaultSubject,
+			},
+		},
+	})
+
+	hs := hatserver.NewServer()
+
+	req := &hatspb.MakeHatsRequest{
+		Size:     DefaultSize,
+		Style:    DefaultStyle,
+		Color:    DefaultColor,
+		Quantity: DefaultQuantity,
+		Notes:    DefaultNotes,
+	}
+
+	return ctx, hs, req
+}
+*/
+
+func TestCreate(t *testing.T) {
+
+	// connect mongo
+	mongoClient, err := mongodb.Client()
+	if err != nil {
+		t.Fatalf("%s", err)
+	}
+	defer mongoClient.Disconnect(context.Background())
+
+	dao := hatdao.New(mongoClient)
+
+	t.Logf("XXXXX %#v", mongoClient)
+	t.Logf("XXXXX %#v", dao)
+
+}
--- devops/scripts/helm-upgrade.sh
+++ devops/scripts/helm-upgrade.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 set -exuo pipefail
 
-#sops -d ./devops/helmchart/local.sops.yaml > ./devops/helmchart/local.plain.yaml
+NAMESPACE=$1
 
-helm upgrade --install scorpicode ./devops/helmchart \
--f devops/helmchart/dev.yaml \
+# TODO: add logic, if NAMESPACE is empty, use "default"
+
+# TODO: integrate SOPS with Github Actions
+#sops -d ./devops/helmchart/${NAMESPACE}.sops.yaml > ./devops/helmchart/${NAMESPACE}.plain.yaml
+
+helm upgrade --install -n ${NAMESPACE} scorpicode ./devops/helmchart \
+-f devops/helmchart/${NAMESPACE}.yaml \
+-f devops/helmchart/tags.yaml \
 --set common.auth0RedirectUri=http://`minikube ip`:30080/callback \
 --set roxie.loginSuccessTarget=http://`minikube ip`:30080/sc \
 --set frontend.socketHost=`minikube ip`:30081
