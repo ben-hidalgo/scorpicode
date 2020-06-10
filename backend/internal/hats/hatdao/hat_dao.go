1d1a6eb4ef692c5d3698d8af32fdf3b03063f925
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-06-10 15:21:24 -0700

    combined multiple files in hatdao and rabbit package folders

--- backend/internal/hats/hatdao/hat_dao.go
+++ backend/internal/hats/hatdao/hat_dao.go
@@ -2,8 +2,10 @@ package hatdao
 
 import (
 	"context"
+	"encoding/hex"
 
 	"github.com/Kamva/mgm/v2"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -62,4 +64,64 @@ func From(ctx context.Context) HatDao {
 	default:
 		panic("hatdao.From() no value found")
 	}
+}
+
+// Create .
+func (i *impl) Create(ctx context.Context, e *Hat) error {
+	e.Version = 1
+	return mgm.Coll(e).CreateWithCtx(ctx, e)
+}
+
+// Delete .
+func (i *impl) Delete(ctx context.Context, e *Hat) error {
+	return mgm.Coll(e).DeleteWithCtx(ctx, e)
+}
+
+// Find not found returns nil, nil
+func (i *impl) Find(ctx context.Context, id string) (*Hat, error) {
+
+	h := &Hat{}
+
+	coll := mgm.Coll(h)
+
+	err := coll.FindByIDWithCtx(ctx, id, h)
+	if err == hex.ErrLength || err == mongo.ErrNoDocuments {
+		// malformed id means not found
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return h, nil
+}
+
+// Query .
+func (i *impl) Query(ctx context.Context) ([]*Hat, error) {
+
+	// result := []Book{}
+	// err := mgm.Coll(&Book{}).SimpleFind(&result, bson.M{"age": bson.M{operator.Gt: 24}})
+
+	results := []*Hat{}
+
+	err := mgm.Coll(&Hat{}).SimpleFindWithCtx(ctx, &results, bson.M{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return results, nil
+}
+
+// VisitTxn .
+func (i *impl) VisitTxn(ctx context.Context, tf func() error) error {
+
+	return mgm.TransactionWithCtx(ctx, func(session mongo.Session, sc mongo.SessionContext) error {
+
+		err := tf()
+		if err != nil {
+			return err
+		}
+
+		return session.CommitTransaction(sc)
+	})
 }
--- backend/pkg/rabbit/rabbit.go
+++ backend/pkg/rabbit/rabbit.go
@@ -4,6 +4,7 @@ import (
 	"backend/pkg/envconfig"
 	"bytes"
 	"context"
+	"encoding/json"
 
 	"github.com/socifi/jazz"
 	"github.com/twitchtv/twirp"
@@ -96,4 +97,29 @@ func ServerHooks(jc *jazz.Connection) *twirp.ServerHooks {
 			return context.WithValue(ctx, Key, New(jc)), nil
 		},
 	}
+}
+
+//impl .
+type impl struct {
+	Conn *jazz.Connection
+}
+
+// Send .
+func (i *impl) Send(ex Exchange, key RKey, msg string) error {
+	return i.Conn.SendMessage(string(ex), string(key), msg)
+}
+
+// SendBlob .
+func (i *impl) SendBlob(ex Exchange, key RKey, msg []byte) error {
+	return i.Conn.SendBlob(string(ex), string(key), msg)
+}
+
+// SendJSON .
+func (i *impl) SendJSON(ex Exchange, key RKey, msg interface{}) error {
+
+	s, err := json.Marshal(msg)
+	if err != nil {
+		return err
+	}
+	return i.SendBlob(ex, key, s)
 }
