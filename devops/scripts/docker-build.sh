029d15d16cfbbf0701be59ae0f5e77a6d74eebd9
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-06-18 16:46:11 -0700

    added sleep and docker images debug

--- devops/scripts/docker-build.sh
+++ devops/scripts/docker-build.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 set -exuo pipefail
 
+eval $(minikube docker-env)
+
 docker build . -f devops/dockerfiles/hats.Dockerfile     -t hats:`./devops/scripts/go-checksum.sh hats`
 docker build . -f devops/dockerfiles/website.Dockerfile  -t website:`./devops/scripts/js-checksum.sh website`
 docker build . -f devops/dockerfiles/frontend.Dockerfile -t frontend:`./devops/scripts/js-checksum.sh frontend`
 docker build . -f devops/dockerfiles/roxie.Dockerfile    -t roxie:`./devops/scripts/go-checksum.sh roxie`
 docker build . -f devops/dockerfiles/soxie.Dockerfile    -t soxie:`./devops/scripts/go-checksum.sh soxie`
 docker build . -f devops/dockerfiles/debugger.Dockerfile -t debugger:latest
+
+docker images
