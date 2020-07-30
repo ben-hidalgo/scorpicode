211b5d7fb540c3bed7e873015c5a22bf9d13cdcf
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-07-29 18:03:47 -0700

    added defaulting logic for branches and multiple engineers to helm upgrade

--- devops/scripts/helm-upgrade.sh
+++ devops/scripts/helm-upgrade.sh
@@ -3,15 +3,30 @@ set -exuo pipefail
 
 NAMESPACE=$1
 
-# TODO: add logic, if NAMESPACE is `whoami` use "cloud.yaml" but `whoami` namespace
-# TODO: add logic for cloud.yaml in GitHub actions namespace is branch name
+TARGET_DIR=./devops/helmchart
+NAMESPACE_YAML="${TARGET_DIR}/${NAMESPACE}.yaml"
+TAGS_YAML="${TARGET_DIR}/tags.yaml"
+
+# if a yaml specific to the passed in namespace is found, use it
+if test -f $NAMESPACE_YAML; then
+    VALUES_YAML=$NAMESPACE_YAML
+    SOPS_YAML="${TARGET_DIR}/${NAMESPACE}.sops.yaml"
+    PLAIN_YAML="${TARGET_DIR}/${NAMESPACE}.plain.yaml"
+else
+    # otherwise, use the default for gcp
+    VALUES_YAML="${TARGET_DIR}/_gcp-default.yaml"
+    SOPS_YAML="${TARGET_DIR}/_gcp-default.sops.yaml"
+    PLAIN_YAML="${TARGET_DIR}/_gcp-default.plain.yaml"
+fi
 
 # TODO: integrate SOPS with Github Actions
-#sops -d ./devops/helmchart/${NAMESPACE}.sops.yaml > ./devops/helmchart/${NAMESPACE}.plain.yaml
+sops -d $SOPS_YAML > $PLAIN_YAML
 
-kubectl create namespace ${NAMESPACE} || true
+kubectl create namespace $NAMESPACE || true
 
-helm upgrade --install -n ${NAMESPACE} scorpicode ./devops/helmchart \
--f devops/helmchart/${NAMESPACE}.yaml \
--f devops/helmchart/tags.yaml \
--f devops/helmchart/local.plain.yaml
+# TODO: helm still doesn't seem to support STDIN to avoid writing plain.yaml
+helm upgrade --install scorpicode $TARGET_DIR \
+-n $NAMESPACE \
+-f $TAGS_YAML \
+-f $VALUES_YAML \
+-f $PLAIN_YAML
