66f8bf96c5bc04056088c22c56e945eecf5afc2d
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-09-10 16:46:27 -0700

    switched integration tests from Minikube to Terraform

--- devops/scripts/integration-tests.sh
+++ devops/scripts/integration-tests.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -exuo pipefail
 
-# var MongoURI = "mongodb://scuser:scpass@localhost:27017/scdata"
-# var AmqpDsn = "amqp://rabbit:rabbit@localhost:5672/"
-
 NAMESPACE=$1
 
 (cd backend && \
-AMQP_DSN=amqp://scuser:scpass@`minikube ip`:`kubectl get svc scorpicode-rabbitmq-ha -n ${NAMESPACE} -o json | jq '.spec.ports[1].nodePort'`/schost \
-MONGO_URI=mongodb://scuser:scpass@`minikube ip`:`kubectl get svc scorpicode-mongodb -n ${NAMESPACE} -o json | jq '.spec.ports[0].nodePort'`/scdata \
+MONGO_URI=mongodb://localhost:27017 \
+AMQP_DSN=amqp://guest:guest@localhost:5672/ \
 MONGO_DB=scdata \
 go test ./... -v -count=1)
