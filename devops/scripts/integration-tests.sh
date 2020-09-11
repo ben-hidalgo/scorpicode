b2336989c5c7640d1e6fa23ff921b0e44eb5a4c7
Ben Hidalgo <ben-hidalgo@users.noreply.github.com>
2020-09-10 17:25:16 -0700

    debugging rabbit connection error in Actions

--- devops/scripts/integration-tests.sh
+++ devops/scripts/integration-tests.sh
@@ -4,7 +4,7 @@ set -exuo pipefail
 NAMESPACE=$1
 
 (cd backend && \
-MONGO_URI=mongodb://localhost:27017 \
-AMQP_DSN=amqp://guest:guest@localhost:5672/ \
+MONGO_URI=mongodb://127.0.0.1:27017 \
+AMQP_DSN=amqp://guest:guest@127.0.0.1:5672/ \
 MONGO_DB=scdata \
 go test ./... -v -count=1)
